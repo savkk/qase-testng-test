25e8c9f529e156e7cda00550497d190278b3c284
savkk <savkk@bk.ru>
2020-01-28 01:59:58 +0300

    sleep

--- src/test/java/io/qase/testngtest/TestClass.java
+++ src/test/java/io/qase/testngtest/TestClass.java
@@ -3,15 +3,20 @@ package io.qase.testngtest;
 import io.qase.testng.CaseId;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+import java.util.Random;
+
 public class TestClass {
     @Test
     @CaseId(1)
-    public void success() {
+    public void success() throws InterruptedException {
+        Thread.sleep(Duration.ofSeconds(new Random().nextInt(10)).toMillis());
     }
 
     @Test
     @CaseId(2)
-    public void failure() {
+    public void failure() throws InterruptedException {
+        Thread.sleep(Duration.ofSeconds(new Random().nextInt(10)).toMillis());
         throw new AssertionError();
     }
 }
